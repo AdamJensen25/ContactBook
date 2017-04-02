909d1669c0c6ea94c6e18b3554a4bc092000095c
Adam Jensen <mandrake2410>
2017-04-02 21:07:46 +0300

    added Contacts DB Contract and OpenHelper

---
+++ app/src/main/java/ua/vladprischepa/contactbooktesttask/database/ContactsContract.java
@@ -0,0 +1,53 @@
+package ua.vladprischepa.contactbooktesttask.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * @author Vlad Prischepa
+ * @since 02.04.2017
+ * @version 1
+ */
+
+public final class ContactsContract{
+
+    public static final String COLUMN_ID = BaseColumns._ID;
+    public static final String COLUMN_CONTACT_ID = "contact_id";
+
+    public static final class ContactsTableEntry{
+
+        public static final String TABLE_NAME = "Contacts";
+        public static final String COLUMN_FIRST_NAME = "first_name";
+        public static final String COLUMN_LAST_NAME = "last_name";
+    }
+
+    public static final class PhonesTableEntry{
+
+        public static final String TABLE_NAME = "Phones";
+
+
+        public static final String COLUMN_PHONE_NUMBER = "number";
+        public static final String COLUMN_NUMBER_TYPE = "type";
+
+        public static final int PHONE_TYPE_MOBILE = 0;
+        public static final int PHONE_TYPE_HOME = 1;
+        public static final int PHONE_TYPE_WORK = 2;
+        public static final int PHONE_TYPE_MAIN = 3;
+        public static final int PHONE_TYPE_WORK_FAX = 4;
+        public static final int PHONE_TYPE_HOME_FAX = 5;
+        public static final int PHONE_TYPE_PAGER = 6;
+        public static final int PHONE_TYPE_OTHER = 7;
+    }
+
+    public static final class EmailTableEntry{
+        public static final String TABLE_NAME = "Emails";
+
+        public static final String COLUMN_EMAIL = "email";
+        public static final String COLUMN_EMAIL_TYPE = "type";
+
+        public static final int EMAIL_TYPE_PERSONAL = 0;
+        public static final int EMAIL_TYPE_WORK = 1;
+        public static final int EMAIL_TYPE_OTHER = 2;
+    }
+
+
+}
---
+++ app/src/main/java/ua/vladprischepa/contactbooktesttask/database/ContactsDBHelper.java
@@ -0,0 +1,73 @@
+package ua.vladprischepa.contactbooktesttask.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
+/**
+ * @author Vlad Prischepa
+ * @since 02.04.2017
+ * @version 1
+ */
+
+public class ContactsDBHelper extends SQLiteOpenHelper{
+
+    /**
+     * Database version, should be incremented within new version of database;
+     */
+    private static final int DB_VERSION = 1;
+    /**
+     * Database name suffix used with database name;
+     */
+    private static final String DB_NAME_SUFFIX = ".db";
+
+    /**
+     *  public constructor for {@link ContactsDBHelper}
+     * @param context Application context
+     * @param account Google Account, used as database name
+     */
+    public ContactsDBHelper(Context context, GoogleSignInAccount account) {
+        super(context, account.getEmail()+DB_NAME_SUFFIX, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(SQL_CREATE_TABLE_CONTACTS);
+        db.execSQL(SQL_CREATE_TABLE_PHONES);
+        db.execSQL(SQL_CREATE_TABLE_EMAILS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(SQL_DROP_TABLE+ContactsContract.ContactsTableEntry.TABLE_NAME);
+        db.execSQL(SQL_DROP_TABLE+ContactsContract.PhonesTableEntry.TABLE_NAME);
+        db.execSQL(SQL_DROP_TABLE+ContactsContract.EmailTableEntry.TABLE_NAME);
+        onCreate(db);
+    }
+
+    private static final String SQL_CREATE_TABLE_CONTACTS = "CREATE TABLE IF NOT EXISTS"
+            + ContactsContract.ContactsTableEntry.TABLE_NAME + "("
+            + ContactsContract.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ContactsContract.ContactsTableEntry.COLUMN_FIRST_NAME + " TEXT NOT NULL, "
+            + ContactsContract.ContactsTableEntry.COLUMN_LAST_NAME + " TEXT NOT NULL" + ");";
+
+    private static final String SQL_CREATE_TABLE_PHONES = "CREATE TABLE IF NOT EXISTS"
+            + ContactsContract.PhonesTableEntry.TABLE_NAME + "("
+            + ContactsContract.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ContactsContract.COLUMN_CONTACT_ID + " INTEGER NOT NULL, "
+            + ContactsContract.PhonesTableEntry.COLUMN_PHONE_NUMBER + " TEXT NOT NULL, "
+            + ContactsContract.PhonesTableEntry.COLUMN_NUMBER_TYPE + " INTEGER NOT NULL DEFAULT 0"
+            + ");";
+    private static final String SQL_CREATE_TABLE_EMAILS = "CREATE TABLE IF NOT EXISTS"
+            + ContactsContract.EmailTableEntry.TABLE_NAME + "("
+            + ContactsContract.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + ContactsContract.COLUMN_CONTACT_ID + " INTEGER NOT NULL, "
+            + ContactsContract.EmailTableEntry.COLUMN_EMAIL + " TEXT NOT NULL, "
+            + ContactsContract.EmailTableEntry.COLUMN_EMAIL_TYPE + " INTEGER NOT NULL DEFAULT 0"
+            + ");";
+
+    private static final String SQL_DROP_TABLE = "DROP TABLE IF EXISTS ";
+
+}
